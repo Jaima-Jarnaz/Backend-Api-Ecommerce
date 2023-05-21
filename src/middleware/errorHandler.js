1f0e2fc2a4ce3cf352d96367a0698db1882da839
Jaima07 <jaima.jarnaz@bjitgroup.com>
2023-05-21 22:28:50 +0600

    added handler

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,20 @@
+const ErrorHandlerClass = require("../utils/errorHandlerClass");
+
+module.exports = (err, req, res, next) => {
+  err.message = err.message || "Sorry!!Internal Server Error";
+  err.statusCode = err.statusCode || 500;
+
+  //mongodb id error and CastError: Cast to number failed for value "20000" (type string) at path "price" for model "Product"
+  //this type error
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid ${err.path} and ${err}`;
+    err = new ErrorHandlerClass(message, 400);
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+    status: err.statusCode,
+    error: err.stack,
+  });
+};
