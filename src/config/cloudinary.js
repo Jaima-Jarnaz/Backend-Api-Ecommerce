c8230ee3414ff3549a687390acba947a787771fe
Jaima07 <jaima.jarnaz@bjitgroup.com>
2023-05-28 21:43:10 +0600

    cloudinary added

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,11 +1,10 @@
- const cloudinary = require('cloudinary').v2;
+const cloudinary = require("cloudinary").v2;
 
- // Cloudinary Configuration 
-  cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-  });
+// Cloudinary Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-  module.exports=cloudinary
-
+module.exports = cloudinary;
