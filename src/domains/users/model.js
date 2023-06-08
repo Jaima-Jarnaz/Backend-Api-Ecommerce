76370b29eacd30df3c823ab2c8e124cabe0f3f3f
Jaima Jarnaz <91679098+Jaima07@users.noreply.github.com>
2023-06-08 17:05:57 +0900

    user packages added with error

--- src/domains/users/model.js
+++ src/domains/users/model.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
@@ -22,9 +24,65 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    roles: {
+      type: [String],
+      enum: ["user", "admin"],
+      default: ["user"],
+    },
+    token: {
+      type: String,
+      required: true,
+    },
   },
   { collation: "user" }
 );
+
+userSchema.pre("save", function (next) {
+  let user = this;
+  if (user.isModified("password")) {
+    bcrypt.genSalt("SALT_I", function (err, salt) {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(user.password, salt, function (err, salt) {
+        if (err) {
+          return next(err);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+userSchema.methods.comparePassword = function (givenPassword, cb) {
+  bcrypt.compare(givenPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), process.env.TOKEN_SECRET);
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
+  jwt.verify(token, process.env.TOKEN_SECRET, function (err, decode) {
+    user.findOne({ _id: decode, token: token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+};
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
