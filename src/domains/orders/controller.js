11b219a821ab34dab2c41f1ca5fe2f71815a1ac1
Jaima Jarnaz <jaima.jarnaz@bjitgroup.com>
2023-12-18 17:24:14 +0600

    get update api implemented

--- src/domains/orders/controller.js
+++ src/domains/orders/controller.js
@@ -1,11 +1,10 @@
 const Order = require("./model");
 const CustomErrorHandler = require("../../utils/customErrorHandler");
 
+//------------API for create orders-----------------
 const createOrder = async (req, res, next) => {
   try {
-    const { deliveryAddress } = req.body;
     const orderData = new Order(req.body);
-    console.log(deliveryAddress);
     await orderData.save().catch((error) => {
       console.error(error);
     });
@@ -26,6 +25,75 @@ const createOrder = async (req, res, next) => {
   }
 };
 
+//------------API for get all orders----------------
+const getAllOrders = async (req, res, next) => {
+  try {
+    const orders = await Order.find();
+
+    if (!orders) {
+      return next(new CustomErrorHandler("No orders available!!!", 404));
+    }
+
+    res.status(201).send({
+      message: "Orders found successfully",
+      orders,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Sorry!!!Server Error!!!!!!!!!!",
+      error: error,
+    });
+  }
+};
+
+//------------API for get all orders----------------
+const getSingleOrder = async (req, res, next) => {
+  try {
+    const orders = await Order.findById(req.params.id);
+
+    if (!orders) {
+      return next(new CustomErrorHandler("No orders available!!!", 404));
+    }
+
+    res.status(201).send({
+      message: "Order found successfully",
+      orders,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Sorry!!!Server Error!!!!!!!!!!",
+      error: error,
+    });
+  }
+};
+
+//------------API for update order----------------
+const updateOrderDetails = async (req, res, next) => {
+  try {
+    const orders = await Order.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!orders) {
+      return next(new CustomErrorHandler("No orders available!!!", 404));
+    }
+
+    res.status(201).send({
+      message: "Order data updated successfully",
+      orders,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Sorry!!!Server Error!!!!!!!!!!",
+      error: error,
+    });
+  }
+};
+
 module.exports = {
   createOrder,
+  getAllOrders,
+  getSingleOrder,
+  updateOrderDetails,
 };
--- src/domains/orders/route.js
+++ src/domains/orders/route.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const {
   createOrder,
-  //   getAllProducts,
-  //   getSingleProduct,
-  //   updateProducts,
+  getAllOrders,
+  getSingleOrder,
+  updateOrderDetails,
   //   deleteProduct
 } = require("./controller");
 //const authenticateUser = require("../middleware/auth");
 const router = express.Router();
 
-// router.route("/all").get(getAllProducts);
+router.route("/all").get(getAllOrders);
 router.route("/create").post(createOrder);
-// router.route("/:id").get(getSingleProduct);
-// router.route("/update/:id").put(updateProducts);
+router.route("/:id").get(getSingleOrder);
+router.route("/update/:id").put(updateOrderDetails);
 // router.route("/delete/:id").delete(deleteProduct);
 
 module.exports = router;
